76054fd2ba463dd410de035e0dc390d6e13ac0e7
Alon Grinberg Dana <alongd@mit.edu>
2020-06-15 00:07:41 -0400

    Tests: Added a "no T3 no QM" functional test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -18,6 +18,40 @@ from t3.utils.dependencies import check_dependencies
 functional_minimal_directory = os.path.join(DATA_BASE_PATH, 'functional_minimal_example')
 
 
+def test_no_t3_no_qm():
+    """Test proper execution of T3 without specifying neither of the t3 nor qm args"""
+    rmg_args = {'database': {'thermo_libraries': ['primaryThermoLibrary', 'BurkeH2O2'],
+                             'kinetics_libraries': ['BurkeH2O2inN2']},
+                'species': [{'label': 'H2',
+                             'smiles': '[H][H]',
+                             'concentration': 0.67},
+                            {'label': 'O2',
+                             'smiles': '[O][O]',
+                             'concentration': 0.33}],
+                'reactors': [{'type': 'gas batch constant T P',
+                              'T': 1000,
+                              'P': 1,
+                              'termination_conversion': {'H2': 0.1},
+                              'termination_time': 1e-3}],
+                'model': {'core_tolerance': 0.01}}
+
+    t3_object = T3(project='T3_functional_test_1',
+                   rmg=rmg_args,
+                   clean_dir=True,
+                   )
+    t3_object.execute()
+    with open(os.path.join(t3_object.project_directory, 't3.log'), 'r') as f:
+        lines = f.readlines()
+    for line in ['#                  The   Tandem   Tool   (T3)                  #\n',
+                 'Starting project T3_functional_test_1\n',
+                 'T3 iteration 1:\n',
+                 ]:
+        assert line in lines
+    assert not any('T3 iteration 0:\n' in line for line in lines)
+    assert any('T3 execution terminated' in line for line in lines)
+    shutil.rmtree(t3_object.project_directory)
+
+
 def test_minimal_example():
     """Tests that the minimal example is functional (ARC is not being called)"""
     input_file = os.path.join(functional_minimal_directory, 'input.yml')
@@ -36,9 +70,11 @@ def test_minimal_example():
     # check that the minimal example ran to completion by checking for existence of some files and directories
     assert os.path.isfile(os.path.join(functional_minimal_directory, 't3.log'))
     assert os.path.isdir(os.path.join(functional_minimal_directory, 'iteration_0'))
-    assert os.path.isfile(os.path.join(functional_minimal_directory, 'iteration_0', 'RMG', 'chemkin', 'species_dictionary.txt'))
+    assert os.path.isfile(os.path.join(functional_minimal_directory, 'iteration_0',
+                                       'RMG', 'chemkin', 'species_dictionary.txt'))
     assert os.path.isdir(os.path.join(functional_minimal_directory, 'iteration_1'))
-    assert os.path.isfile(os.path.join(functional_minimal_directory, 'iteration_1', 'RMG', 'chemkin', 'species_dictionary.txt'))
+    assert os.path.isfile(os.path.join(functional_minimal_directory, 'iteration_1',
+                                       'RMG', 'chemkin', 'species_dictionary.txt'))
 
     # remove directories created by this functional test
     log_file = os.path.join(functional_minimal_directory, 't3.log')
