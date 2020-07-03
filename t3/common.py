036d688240df1ed9dde44157b355f213c97532f9
Kevin Spiekermann <kspieker@mit.edu>
2020-07-03 08:28:45 -0400

    Update main.py to reflect use of the simulate adapter
    Run SA using the simulate adapter rather than the previous `run_sa()` method. 
    
    Remove code from `run_sa()` that is now part of the `get_sa_coefficients()` method in the RMG adapter. 
    
    Remove the corresponding imports in main.py that have now been moved to the RMG Simulator Adapter.
    
    Move get_species_by_label() to common.py

--- t3/common.py
+++ t3/common.py
@@ -4,7 +4,9 @@ t3 common module
 
 import os
 import string
+from typing import Union
 
+from rmgpy.species import Species
 
 VERSION = '0.1.0'
 
@@ -12,6 +14,7 @@ t3_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # absolut
 DATA_BASE_PATH = os.path.join(t3_path, 'tests', 'data')
 EXAMPLES_BASE_PATH = os.path.join(t3_path, 'examples')
 PROJECTS_BASE_PATH = os.path.join(t3_path, 'Projects')
+test_minimal_project_directory = os.path.join(PROJECTS_BASE_PATH, 'test_minimal_delete_after_usage')
 VALID_CHARS = "-_=.,%s%s" % (string.ascii_letters, string.digits)
 
 
@@ -47,3 +50,34 @@ def delete_root_rmg_log(project_directory: str) -> None:
     rmg_log_path = os.path.join(project_directory, 'RMG.log')
     if os.path.isfile(rmg_log_path):
         os.remove(rmg_log_path)
+
+
+def get_species_by_label(label: str,
+                         species_list: list,
+                         ) -> Union[Species, None]:
+    """
+    Get a species from a list of species by its label. This label includes the index in parenthesis as assigned
+    in the chemkin file. Ex: H(3)
+
+    Args:
+        label (str): A species' label (species.label).
+        species_list (list): Entries are RMG Species objects.
+
+    Raises:
+        InputError: If ``label`` is None.
+
+    Returns:
+        Union[Species, None]: The corresponding species from the species_list.
+                              Returns ``None`` if no species was found.
+    """
+    if label is None:
+        raise ValueError('Got None as label input')
+    for spc in species_list:
+        if spc.label == label or spc.to_chemkin() == label:
+            return spc
+    if '(' in label and ')' in label:
+        # try by the RMG species index
+        for spc in species_list:
+            if spc.index == int(label.split('(')[-1].split(')')[0]):
+                return spc
+    return None
