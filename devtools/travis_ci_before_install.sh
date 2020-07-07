064da719c4d2a2f4cd2f2583a2c3c3c458ebd33d
Kevin Spiekermann <kspieker@mit.edu>
2020-07-07 17:34:17 -0400

    Update travis yml to allow RMS adapter tests to run on travis
    a

--- devtools/travis_ci_before_install.sh
+++ devtools/travis_ci_before_install.sh
@@ -2,6 +2,22 @@
 pushd .
 cd $HOME
 
+# make temporary directory
+mkdir -p ~/temp_Downloads
+
+# Install Julia
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then curl -L https://julialang-s3.julialang.org/bin/linux/x64/1.1/julia-1.1.0-linux-x86_64.tar.gz -o "$HOME/temp_Downloads/julia.tar.gz"; fi
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then tar xzf "$HOME/temp_Downloads/julia.tar.gz" -C "$HOME/temp_Downloads"; fi
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then cp -r "$(find "$HOME/temp_Downloads" -maxdepth 2 -name "julia*" -type d | head -n 1)" "$HOME/julia"; fi
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then curl -L https://julialang-s3.julialang.org/bin/mac/x64/1.1/julia-1.1.0-mac64.dmg -o "$HOME/temp_Downloads/julia.dmg"; fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then hdiutil attach ~/temp_Downloads/julia.dmg; fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then cp -r /Volumes/Julia*/Julia*/Contents/Resources/julia $HOME/julia; fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then hdiutil detach -force /Volumes/Julia*; fi
+
+rm -rf ~/temp_Downloads/julia*
+export PATH="$HOME/julia/bin:$PATH"
+
 # Install Miniconda
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
