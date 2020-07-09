e186bde454ca36cebbdee73b9acfd2493aede040
Alon Grinberg Dana <alongd@mit.edu>
2020-07-09 16:59:31 -0400

    rms fix

--- Makefile
+++ Makefile
@@ -4,6 +4,20 @@
 #
 ################################################################################
 
+install-rms:
+	cd ..
+    git clone https://github.com/ReactionMechanismGenerator/pyrms
+    cd pyrms
+    git checkout py3
+    conda install -c conda-forge julia
+	julia -v
+    julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2],"/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
+    python devtools/install_pyrms.py
+    source ~/.bashrc
+    python devtools/install_pyrms.jl
+    ln -sfn $(which python-jl) $(which python)
+    cd T3
+
 test:
 	pytest -ra -vv
 
---
+++ devtools/install_pyrms.jl
@@ -0,0 +1,51 @@
+using Pkg
+
+# get the python path
+link_python = false
+if !("PyCall" in keys(Pkg.installed()))
+    Pkg.add("PyCall")
+    link_python = true
+else
+    println("found PyCall importing")
+    v = 0
+    try
+        using PyCall
+        sys = pyimport("sys")
+        v = sys.version_info[1]
+    catch
+        v = -1  # PyCall isn't setup right
+    end
+    if v != 3
+        println("removing PyCall and reinstalling")
+        Pkg.rm("PyCall")
+        if "Conda" in keys(Pkg.installed())
+            Pkg.rm("Conda")
+        end
+        Pkg.add("PyCall")
+        link_python = true
+    end
+end
+
+println("PyCall present")
+if link_python
+    println("Linking PyCall properly")
+    out = Pipe()
+    proc = run(pipeline(`which python`,stdout=out))
+    close(out.in)
+    pypath = chomp(String(read(out)))
+
+    # set env variables for installing PyCall
+    ENV["CONDA_JL_HOME"] = join(split(pypath, "/")[1:end-2], "/")
+    ENV["PYTHON"] = pypath
+    println(ENV["CONDA_JL_HOME"])
+    println(ENV["PYTHON"])
+    Pkg.build("PyCall")
+end
+
+println("adding RMS")
+Pkg.add("DifferentialEquations")
+using DifferentialEquations
+Pkg.add(PackageSpec(url="https://github.com/ReactionMechanismGenerator/ReactionMechanismSimulator.jl", rev="master"))
+println("importing PyCall and RMS")
+using PyCall
+using ReactionMechanismSimulator
