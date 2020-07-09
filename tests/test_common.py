50d80c53c1aa973af779f752d754a06fc8c270b2
Alon Grinberg Dana <alongd@mit.edu>
2020-07-09 09:12:49 -0400

    Tests: common get_rmg_species_from_a_species_dict

--- tests/test_common.py
+++ tests/test_common.py
@@ -6,8 +6,11 @@ t3 tests test_utils module
 """
 import os
 
+from rmgpy.species import Species
+
 import t3.common as common
 from t3.common import DATA_BASE_PATH
+from t3.schema import RMGSpecies
 from tests.common import run_minimal
 
 
@@ -37,3 +40,28 @@ def test_get_species_by_label():
     species = common.get_species_by_label(label, rmg_species)
     assert species.label == label
     assert species.index == 7
+
+
+def test_get_rmg_species_from_a_species_dict():
+    """Test getting an RMG species from a species dictionary"""
+    species = common.get_rmg_species_from_a_species_dict(
+        species_dict=RMGSpecies(**{'label': 'spc', 'smiles': 'C=O'}).dict())
+    assert isinstance(species, Species)
+    assert species.label == 'spc'
+    assert species.molecule[0].to_smiles() == 'C=O'
+
+    adj = """1 C u0 p0 c0 {2,D} {3,S} {4,S}
+2 O u0 p2 c0 {1,D}
+3 H u0 p0 c0 {1,S}
+4 H u0 p0 c0 {1,S}"""
+    species = common.get_rmg_species_from_a_species_dict(
+        species_dict=RMGSpecies(**{'label': 'spc', 'adjlist': adj}).dict())
+    assert isinstance(species, Species)
+    assert species.label == 'spc'
+    assert species.molecule[0].to_smiles() == 'C=O'
+
+    species = common.get_rmg_species_from_a_species_dict(
+        species_dict=RMGSpecies(**{'label': 'spc', 'inchi': 'InChI=1S/CH2O/c1-2/h1H2'}).dict())
+    assert isinstance(species, Species)
+    assert species.label == 'spc'
+    assert species.molecule[0].to_smiles() == 'C=O'
