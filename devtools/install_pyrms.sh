b79a5301d83f90f6b0c466b9dab86327a9b72e79
Alon Grinberg Dana <alongd@mit.edu>
2020-07-09 22:41:14 -0400

    rms

--- devtools/install_pyrms.sh
+++ devtools/install_pyrms.sh
@@ -6,11 +6,11 @@ julia -v
 julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2],"/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
 python devtools/install_pyrms.py
 source ~/.bashrc
-python3 -m pip install julia
+julia devtools/install_pyrms.jl
+# python3 -m pip install julia
 python -c "import julia; julia.install()"
 pip install diffeqpy
 python -c "import diffeqpy; diffeqpy.install()"
-julia devtools/install_pyrms.jl
 
 ln -sfn $(which python-jl) $(which python)
 cd ../T3
