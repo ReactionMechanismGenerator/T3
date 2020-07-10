041913b21e48d754b415643a1dcd002f9e25d4b6
Alon Grinberg Dana <alongd@mit.edu>
2020-07-10 09:44:37 -0400

    rms

--- devtools/install_pyrms.sh
+++ devtools/install_pyrms.sh
@@ -8,12 +8,13 @@ julia -v
 #julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2], "/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
 #echo "DONE installing J"
 echo "*****% python devtools/install_pyrms.py"
+echo $(pwd)
 cd ../T3
-python devtools/install_pyrms.py
+python ../T3/devtools/install_pyrms.py
 echo "*****% source ~/.bashrc"
 source ~/.bashrc
 echo "*****% julia devtools/install_pyrms.jl"
-julia devtools/install_pyrms.jl
+julia ../T3/devtools/install_pyrms.jl
 # python3 -m pip install julia
 echo "*****% python -c import julia; julia.install()
 python -c "import julia; julia.install()"
