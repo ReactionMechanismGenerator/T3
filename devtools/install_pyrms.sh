4d1ad15fac2686ccfdbeb0db52da0f64d66cd6a4
Alon Grinberg Dana <alongd@mit.edu>
2020-07-09 21:54:11 -0400

    rms

--- devtools/install_pyrms.sh
+++ devtools/install_pyrms.sh
@@ -2,7 +2,6 @@ cd ..
 git clone https://github.com/ReactionMechanismGenerator/pyrms
 cd pyrms
 git checkout py3
-conda install -c conda-forge julia
 julia -v
 julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2],"/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
 python devtools/install_pyrms.py
