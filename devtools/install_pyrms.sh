5d6ba2616a1649a82eab08318aa8d063f8354bba
Alon Grinberg Dana <alongd@mit.edu>
2020-07-09 22:57:14 -0400

    rms

--- devtools/install_pyrms.sh
+++ devtools/install_pyrms.sh
@@ -3,7 +3,9 @@ git clone https://github.com/ReactionMechanismGenerator/pyrms
 cd pyrms
 git checkout py3
 julia -v
-julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2],"/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
+#echo "about to install J"
+#julia -e 'out = Pipe(); proc = run(pipeline(`which python`,stdout=out)); close(out.in); pypath = chomp(String(read(out))); ENV["CONDA_JL_HOME"] = join(split(pypath,"/")[1:end-2], "/"); ENV["PYTHON"] = pypath; using Pkg; Pkg.add("PyCall"); Pkg.build("PyCall");'
+#echo "DONE installing J"
 python devtools/install_pyrms.py
 source ~/.bashrc
 julia devtools/install_pyrms.jl
