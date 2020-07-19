8ed21d1eda4b18ef96407140c6d3e836d64b3457
Alon Grinberg Dana <alongd@mit.edu>
2020-07-19 08:18:08 -0400

    f rms

--- devtools/install_pyrms.sh
+++ devtools/install_pyrms.sh
@@ -14,20 +14,26 @@ echo "***** source ~/.bashrc"
 . ~/.bashrc
 echo "***** re-activate t3_env"
 source activate t3_env
-echo "*****% julia devtools/install_pyrms.jl"
+echo "***** call install_pyrms_1.jl"
 julia ../T3/devtools/install_pyrms_1.jl
 cd ../T3
+echo "***** update new environment"
 conda env update -f environment.yml
 cd ../pyrms
+echo "***** call install_pyrms_1.jl"
 julia ../T3/devtools/install_pyrms_2.jl
 # install python-jl (simply called julia)
+echo "***** install python-jl"
 python3 -m pip install julia
-echo "*****% python -c import julia; julia.install()"
+echo "***** python -c import julia; julia.install()"
 python -c "import julia; julia.install()"
+echo "***** pip install diffeqpy"
 pip install diffeqpy
+echo "***** python -c import diffeqpy; diffeqpy.install()"
 python -c "import diffeqpy; diffeqpy.install()"
 
 # uncomment if must
+echo "***** lionk python and python-jl"
 ln -sfn $(which python-jl) $(which python)
 
 # Restore original directory
