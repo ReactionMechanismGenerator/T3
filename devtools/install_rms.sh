37a7f4cade7fa6278823e7cf034e63f186087e11
Alon Grinberg Dana <alongd@mit.edu>
2020-07-20 22:23:01 -0400

    f julia path

--- devtools/install_rms.sh
+++ devtools/install_rms.sh
@@ -15,15 +15,19 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)/pyrms
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)/pyrms" >> ~/.bashrc
 echo "***** python path 1 is:"
 echo $PYTHONPATH
+echo PATH
 cd ../ARC
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)" >> ~/.bashrc
 cd ../RMG-Py
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)" >> ~/.bashrc
+export PATH="$HOME/julia/bin:$PATH"
+echo "export PATH=$HOME/julia/bin:$PATH" >> ~/.bashrc
 cd ../pyrms
 echo "***** python path 1.5 is:"
 echo $PYTHONPATH
+echo PATH
 # echo "***** source ~/.bashrc"
 # . ~/.bashrc
 #echo "***** re-activate t3_env"
