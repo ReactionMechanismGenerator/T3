f2462c22c05cd3fcb7486fe2f3c92810108f8877
Alon Grinberg Dana <alongd@mit.edu>
2020-07-20 20:15:35 -0400

    f rms

--- devtools/travis_ci_before_install.sh
+++ devtools/travis_ci_before_install.sh
@@ -2,24 +2,24 @@
 pushd .
 cd $HOME
 
-# make a temporary directory
-mkdir -p ~/temp_Downloads
-
 # Install Miniconda
-MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh
+else
+    MINICONDA=Miniconda3-latest-Linux-x86_64.sh
+fi
 MINICONDA_HOME=$HOME/miniconda
-wget -q https://repo.anaconda.com/miniconda/$MINICONDA
+wget -q https://repo.continuum.io/miniconda/$MINICONDA
 bash $MINICONDA -b -p $MINICONDA_HOME
 
 # Configure miniconda
 export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
-echo 'export "PATH=$MINICONDA_HOME/bin:$PATH"' >> ~/.bashrc
-    
+echo "export 'PATH=$MINICONDA_HOME/bin:$PATH'" >> ~/.bashrc
+
 conda config --set always_yes yes --set changeps1 no
 conda update --q conda
 conda info -a
-conda init bash
 
 # Restore original directory
 popd || exit
