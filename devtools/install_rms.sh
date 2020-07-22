0ef07d15d4f2c299f514638340a37e5ad7e5a128
Alon Grinberg Dana <alongd@mit.edu>
2020-07-22 09:52:16 -0400

    no diffeqpy

--- devtools/install_rms.sh
+++ devtools/install_rms.sh
@@ -10,11 +10,11 @@ echo "***** install python-jl"
 python3 -m pip install julia
 echo "***** python -c import julia; julia.install()"
 python -c "import julia; julia.install()"
-echo "***** pip install diffeqpy"
-pip install diffeqpy
-echo "***** python -c import diffeqpy; diffeqpy.install()"
-python -c "import diffeqpy; diffeqpy.install()"
-python -c "import Pkg; Pkg.add('PyCall')"
+#echo "***** pip install diffeqpy"
+#pip install diffeqpy
+#echo "***** python -c import diffeqpy; diffeqpy.install()"
+#python -c "import diffeqpy; diffeqpy.install()"
+#python -c "import Pkg; Pkg.add('PyCall')"
 
 # Change python-jl to call python3:
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then sed -i 's|bin/python|bin/python3|g' $(which python-jl); fi
