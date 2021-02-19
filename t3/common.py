223a4ac07a99a6da0395c00c2946a6b08c74a666
Alon Grinberg Dana <alon@technion.ac.il>
2021-02-19 11:37:04 +0200

    Minor: Modified docstring in common

--- t3/common.py
+++ t3/common.py
@@ -31,7 +31,7 @@ def get_species_by_label(label: str,
         species_list (list): Entries are RMG Species objects.
 
     Returns:
-        Optional[Species]: The corresponding species from the species_list.
+        Optional[Species]: The corresponding Species object from the species_list.
                            Returns ``None`` if no species was found.
     """
     for species in species_list:
