96bc8962b68c708bd9ddb2e772d9400a97152490
Alon Grinberg Dana <alon@technion.ac.il>
2022-08-15 23:47:19 +0300

    Knock out RMS simulation adapters

--- t3/simulate/__init__.py
+++ t3/simulate/__init__.py
@@ -2,12 +2,9 @@
 Initialize imports for the simulator modules.
 """
 
-from .adapter import SimulateAdapter
-from .factory import simulate_factory, _registered_simulate_adapters
-from .cantera_constantTP import CanteraConstantTP
-from .cantera_constantHP import CanteraConstantHP
-from .cantera_constantUV import CanteraConstantUV
-from .rmg_constantTP import RMGConstantTP
-from .rms_constantTP import RMSConstantTP
-from .rms_constantHP import RMSConstantHP
-from .rms_constantUV import RMSConstantUV
+from t3.simulate.adapter import SimulateAdapter
+from t3.simulate.factory import simulate_factory, _registered_simulate_adapters
+from t3.simulate.cantera_constantTP import CanteraConstantTP
+from t3.simulate.cantera_constantHP import CanteraConstantHP
+from t3.simulate.cantera_constantUV import CanteraConstantUV
+from t3.simulate.rmg_constantTP import RMGConstantTP
