460af263918be61fa44277b622fe17c04005e7f0
Alon Grinberg Dana <alon@technion.ac.il>
2022-08-21 11:01:02 +0300

    Update the install_arc script, create the rmg_env

--- devtools/install_arc.sh
+++ devtools/install_arc.sh
@@ -1,19 +1,24 @@
+# Properly configure the shell to use 'conda activate'.
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
 # Temporarily change directory to $HOME to install software
 pushd .
 
 cd ..
-# RMG-database
 git clone https://github.com/ReactionMechanismGenerator/RMG-database
-# RMG-Py
 git clone https://github.com/ReactionMechanismGenerator/RMG-Py
 cd RMG-Py
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)" >> ~/.bashrc
-# compile RMG
+mamba env update -n rmg_env -f environment.yml
+conda activate rmg_env
 make
+python -c "import julia; julia.install(); import diffeqpy; diffeqpy.install()"
+julia -e 'using Pkg; Pkg.add(PackageSpec(name="ReactionMechanismSimulator",rev="main")); using ReactionMechanismSimulator;'
+conda deactivate
 
 cd ..
-# ARC
 git clone https://github.com/ReactionMechanismGenerator/ARC
 cd ARC
 export PYTHONPATH=$PYTHONPATH:$(pwd)
