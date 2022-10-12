bc388d92be5ac45e86c5f61bbad8354c2790985f
Alon Grinberg Dana <alon@technion.ac.il>
2022-10-12 06:14:18 +0300

    Added the submit module with an RMG submit script for HTCondor and SLURM

---
+++ t3/settings/submit.py
@@ -0,0 +1,60 @@
+"""
+Submit scripts
+"""
+
+# Submission scripts stored as a dictionary with software as the primary key.
+submit_scripts = {
+#     'rmg': """#!/bin/bash -l
+# #SBATCH -J {name}
+# #SBATCH -t 05-00:00:00
+# #SBATCH -o out.txt
+# #SBATCH -e err.txt
+# #SBATCH --ntasks={cpus}
+# #SBATCH --mem-per-cpu=9500
+#
+#
+# export PYTHONPATH=$PYTHONPATH:~/Code/RMG-Py/
+#
+# conda activate rmg_env
+#
+# touch initial_time
+#
+# python-jl ~/Code/RMG-Py/rmg.py -n {cpus} input.py
+#
+# touch final_time
+#
+# """,
+    'rmg': """Universe      = vanilla
+
++JobName      = "{name}"
+
+log           = job.log
+output        = out.txt
+error         = err.txt
+
+getenv        = True
+
+should_transfer_files = no
+
+executable = job.sh
+
+request_cpus  = {cpus}
+request_memory = {memory}MB
+
+queue
+
+""",
+    'rmg_job': """#!/bin/bash -l
+
+touch initial_time
+
+source /srv01/technion/$USER/.bashrc
+
+conda activate rmg_env
+
+python-jl /Local/ce_dana/Code/RMG-Py/rmg.py -n {cpus} input.py{max_iterations}
+
+touch final_time
+
+""",
+}
