cc28c13a2d01a43ca9038e27a22b9c055aa499ff
Alon Grinberg Dana <alon@technion.ac.il>
2022-10-19 14:37:05 +0300

    TODO: Tests: Added test for RMG runner

---
+++ tests/test_rmg_runner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+t3 tests test_rmg_runner module
+"""
+
+import os
+
+from arc.common import read_yaml_file
+
+from t3.common import DATA_BASE_PATH, EXAMPLES_BASE_PATH
+from t3.schema import InputBase, RMG, T3
+from t3.utils.writer import to_camel_case, write_rmg_input_file
+
+
+def test_write_submit_script():
+    """Test the write_submit_script() function."""
+    pass
+
+
