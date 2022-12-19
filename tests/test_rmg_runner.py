a80cc698d232bc87a24632a12f7d464b5d87485e
Calvin <calvinpieters@gmail.com>
2022-12-19 13:52:38 +0200

    Updated test_rmg_runner.py
    test_rmg_runner.py: Updated the class TestWriteSubmitScript with a function that runs executes t3 as a 'local' job, and then after 'converging' the function will ensure that the job.sh and submit.sub files are written as expected.

--- tests/test_rmg_runner.py
+++ tests/test_rmg_runner.py
@@ -9,7 +9,8 @@ import os
 from t3.common import DATA_BASE_PATH, EXAMPLES_BASE_PATH
 from t3.runners.rmg_runner import write_submit_script
 import pytest
-
+from tests.common import run_minimal
+import shutil
 
 class TestWriteSubmitScript(object):
 
@@ -192,15 +193,84 @@ queue
         t3_proj_name = "T3_test_name"
         cpus = 8
         max_iter = "-m 100"
-        mem = 16230 #in MB - This is to be incorrect
+        mem = 850 #in MB - This is to be incorrect
         #########################################
 
-        with pytest.raises(AssertionError) as assertion_error:
-                    actual = write_submit_script(project_directory_path,
+        with pytest.raises(ValueError) as value_error:
+                    write_submit_script(project_directory_path,
                                     cpus=cpus,
                                     memory=mem, #in MB
                                     verbose="-v 20",
                                     max_iterations=max_iter,
                                     t3_project_name= t3_proj_name)
         
-        assert 'Memory is in MB and must be between 8000 and 32000' or "Memory is in MB and must be divisable by 1000" in str(assertion_error.value)
+        assert "Memory is in MB and it is strongly recommended to set the memory above 1000" in str(value_error.value)
+        
+        os.remove(os.path.join(project_directory_path,"job.sh"))
+        os.remove(os.path.join(project_directory_path,"submit.sub"))
+        
+    def test_run_rmg_into_write_submit_script(self):
+        
+        
+        
+        t3 = run_minimal(iteration=1, set_paths=True)
+        t3.rmg['rmg_execution_type'] = 'local'
+        
+        rmg_base_path = os.path.join(t3.project_directory, 'iteration_1','RMG')
+        
+        t3.schema['t3']['options']['max_RMG_exceptions_allowed']= 0
+        os.makedirs(rmg_base_path)
+        with open(os.path.join(rmg_base_path,'RMG.log'), 'w') as rmg_log:
+            rmg_log.write("""MODEL GENERATION COMPLETED
+                          """)
+        rmg_log.close()
+        
+        
+        t3.run_rmg()
+
+        
+        expected_bash = """#!/bin/bash -l
+
+touch initial_time
+
+source /srv01/technion/$USER/.bashrc
+
+conda activate rmg_env
+
+python-jl /Local/ce_dana/Code/RMG-Py/rmg.py -n 10 input.py
+
+touch final_time
+
+"""
+        expected_submit = """Universe      = vanilla
+
++JobName      = "T3_minimal_example_RMG"
+
+log           = job.log
+output        = out.txt
+error         = err.txt
+
+getenv        = True
+
+should_transfer_files = no
+
+executable = job.sh
+
+request_cpus  = 10
+request_memory = 25000MB
+
+queue
+
+"""
+
+        assert os.path.isfile(os.path.join(rmg_base_path,"job.sh")) == True
+        assert os.path.isfile(os.path.join(rmg_base_path, "submit.sub")) == True
+        with open(os.path.join(rmg_base_path,"job.sh"),"r") as bash_file:
+            content_bash = bash_file.read()
+        assert content_bash == expected_bash
+
+        with open(os.path.join(rmg_base_path,"submit.sub"),"r") as submit_file:
+            content_submit = submit_file.read()
+        assert content_submit == expected_submit
+        
+        shutil.rmtree(os.path.join(t3.project_directory))
