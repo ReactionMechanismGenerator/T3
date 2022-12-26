e3780f805025c28e6be6dfed89af5b163515d62a
Alon Grinberg Dana <alon@technion.ac.il>
2022-12-26 07:09:31 +0200

    Fix local_submit_scripts definition in imports
    Make sure it is first in the system path
    d! imports
    
    d!
    
    f! imports
    
    f! imports
    
    f! imports

--- t3/imports.py
+++ t3/imports.py
@@ -15,7 +15,7 @@ else:
     pass
 
 
-# Common imports where the user can optionally put a modified copy of settings.py or submit.py file under ~/.t3
+# Common imports where the user can optionally put a modified copy of settings.py or t3_submit.py file under ~/.t3
 home = os.getenv("HOME") or os.path.expanduser("~")
 local_t3_path = os.path.join(home, '.t3')
 
@@ -33,14 +33,14 @@ if os.path.isfile(local_t3_settings_path):
         local_settings_dict = {key: val for key, val in vars(local_settings).items() if '__' not in key}
         settings.update(local_settings_dict)
 
-local_t3_submit_path = os.path.join(local_t3_path, 'submit.py')
+local_t3_submit_path = os.path.join(local_t3_path, 't3_submit.py')
 if os.path.isfile(local_t3_submit_path):
-    local_submit_scripts = dict(), dict(), dict()
-    if local_t3_path not in sys.path:
-        sys.path.insert(1, local_t3_path)
+    local_submit_scripts = dict()
+    if local_t3_path != sys.path[0]:
+        sys.path.insert(0, local_t3_path)
     try:
-        from submit import submit_scripts as local_submit_scripts
+        from t3_submit import submit_scripts as local_submit_scripts
     except ImportError:
-        print('No local .t3/submit.py file found')
+        print('No local .t3/t3_submit.py file found')
     if local_submit_scripts:
         submit_scripts.update(local_submit_scripts)
