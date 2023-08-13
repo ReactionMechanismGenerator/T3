557464d2e6e02d3334b70120c3630b961ef81724
Alon Grinberg Dana <alon@technion.ac.il>
2023-08-13 14:36:30 +0300

    Tests: get_species_concentration_lists_from_ranged_params() case 3

---
+++ tests/test_simulate_adapters/data/rmg_simulator_test_ranges_3a/iteration_1/RMG/input.py
@@ -0,0 +1,66 @@
+database(
+    thermoLibraries=['primaryThermoLibrary'],
+    reactionLibraries=[],
+    transportLibraries=['OneDMinN2', 'PrimaryTransportLibrary', 'NOx2018', 'GRI-Mech'],
+    seedMechanisms=[],
+    kineticsDepositories='default',
+    kineticsFamilies='default',
+    kineticsEstimator='rate rules',
+)
+
+species(
+    label='fuel',
+    reactive=True,
+    structure=SMILES('CC'),
+)
+
+species(
+    label='O2',
+    reactive=True,
+    structure=SMILES('[O][O]'),
+)
+
+species(
+    label='N2',
+    reactive=False,
+    structure=SMILES('N#N'),
+)
+
+simpleReactor(
+    temperature=(1000.0, 'K'),
+    pressure=(1.0, 'bar'),
+    initialMoleFractions={
+        'N2': [14.1, 42.3],
+        'O2': [3.75, 11.25],
+        'fuel': 1.0,
+    },
+    terminationRateRatio=0.1,
+    nSims=12,
+)
+
+model(
+    toleranceMoveToCore=0.01,
+    toleranceInterruptSimulation=0.01,
+    filterReactions=True,
+    filterThreshold=100000000.0,
+    maxNumObjsPerIter=1,
+    terminateAtMaxObjects=False,
+)
+
+simulator(atol=1e-16, rtol=1e-08, sens_atol=1e-06, sens_rtol=0.0001)
+
+options(
+    name='Seed',
+    generateSeedEachIteration=True,
+    saveSeedToDatabase=False,
+    units='si',
+    generateOutputHTML=False,
+    generatePlots=False,
+    saveSimulationProfiles=False,
+    verboseComments=False,
+    saveEdgeSpecies=True,
+    keepIrreversible=False,
+    trimolecularProductReversible=True,
+    wallTime='00:00:05:00',
+    saveSeedModulus=-1,
+)
--- tests/test_simulate_adapters/test_rmg_constantTP.py
+++ tests/test_simulate_adapters/test_rmg_constantTP.py
@@ -10,18 +10,38 @@ import shutil
 
 from t3.common import SIMULATE_DATA_BASE_PATH
 from tests.common import run_minimal
+from t3.main import T3
 from t3.simulate.rmg_constantTP import RMGConstantTP
 
 
-TEST_DIR = os.path.join(SIMULATE_DATA_BASE_PATH, 'rmg_simulator_test')
-
+TEST_DIR_1 = os.path.join(SIMULATE_DATA_BASE_PATH, 'rmg_simulator_test')
+TEST_DIR_2 = os.path.join(SIMULATE_DATA_BASE_PATH, 'rmg_simulator_test_ranges_3a')
+TEST_DIR_3 = os.path.join(SIMULATE_DATA_BASE_PATH, 'rmg_simulator_test_ranges_3b')
+RANGED_INPUT_DICT_1 = {'verbose': 10, 'project_directory': TEST_DIR_2,
+                       'project': 'test_get_species_concentration_lists_from_ranged_params_case_3a',
+                       't3': {'options': {'max_T3_iterations': 2, 'max_RMG_walltime': '00:00:05:00'},
+                              'sensitivity': {'adapter': 'RMGConstantTP', 'top_SA_species': 10}},
+                       'rmg': {'database': {'thermo_libraries': ['primaryThermoLibrary'], 'kinetics_libraries': []},
+                               'reactors': [{'type': 'gas batch constant T P',
+                                             'T': 1000, 'P': 1, 'termination_rate_ratio': 0.1}],
+                               'model': {'core_tolerance': [0.01, 0.001]}},
+                       'qm': {'adapter': 'ARC', 'level_of_theory': 'b3lyp/6-31g(d,p)'}}
+RANGED_INPUT_DICT_2 = {'verbose': 10, 'project_directory': TEST_DIR_3,
+                       'project': 'test_get_species_concentration_lists_from_ranged_params_case_3b',
+                       't3': {'options': {'max_T3_iterations': 2, 'max_RMG_walltime': '00:00:05:00'},
+                              'sensitivity': {'adapter': 'RMGConstantTP', 'top_SA_species': 10}},
+                       'rmg': {'database': {'thermo_libraries': ['primaryThermoLibrary'], 'kinetics_libraries': []},
+                               'reactors': [{'type': 'gas batch constant T P',
+                                             'T': 1000, 'P': 1, 'termination_rate_ratio': 0.1}],
+                               'model': {'core_tolerance': [0.01, 0.001]}},
+                       'qm': {'adapter': 'ARC', 'level_of_theory': 'b3lyp/6-31g(d,p)'}}
 
 def test_set_up_no_sa():
     """
     Run RMG's minimal example without SA by testing the `set_up` method within the RMGSimulator init method.
     By setting observable_list = list(), no SA is run. Instead, RMG is just used to simulate the mechanism.
     """
-    t3 = run_minimal(project_directory=TEST_DIR)
+    t3 = run_minimal(project_directory=TEST_DIR_1)
     t3.set_paths()
 
     rmg_simulator_adapter = RMGConstantTP(t3=t3.t3,
@@ -44,7 +64,7 @@ def test_get_sa_coefficients():
     Then run the `get_sa_coefficients()` method to test that RMGConstantTP correctly parses the SA csv files
     to obtain sa_dict.
     """
-    t3 = run_minimal(project_directory=TEST_DIR)
+    t3 = run_minimal(project_directory=TEST_DIR_1)
     t3.set_paths()
     observable_list = ['OH', 'H']
     rmg_simulator_adapter = RMGConstantTP(t3=t3.t3,
@@ -80,7 +100,7 @@ def test_get_idt_by_T():
     Calculate the ignition delay time for RMG's minimal example.
     Since this adapter simulates at constant T, this method returns a dictionary whose values are empty lists.
     """
-    t3 = run_minimal(project_directory=TEST_DIR)
+    t3 = run_minimal(project_directory=TEST_DIR_1)
     t3.set_paths()
     rmg_simulator_adapter = RMGConstantTP(t3=t3.t3,
                                           rmg=t3.rmg,
@@ -96,20 +116,85 @@ def test_get_idt_by_T():
     assert len(idt_dict['idt_index']) == 0
 
 
+def test_get_species_concentration_lists_from_ranged_params_case_3():
+    """
+    Test the get_species_concentration_lists_from_ranged_params() method
+    For case 3 where ``modify_concentration_ranges_together`` is set to ``True``
+    """
+    input_dict = RANGED_INPUT_DICT_1.copy()
+    input_dict['rmg']['species'] = [
+        {'label': 'fuel', 'smiles': 'CC', 'concentration': 1, 'reactive': True, 'balance': False},
+        {'label': 'O2', 'smiles': '[O][O]', 'concentration': [3.75, 11.25], 'reactive': True, 'balance': False},
+        {'label': 'N2', 'smiles': 'N#N', 'concentration': [14.1, 42.3], 'reactive': False, 'balance': False}]
+    input_dict['t3']['options']['modify_concentration_ranges_together'] = True
+    input_dict['t3']['options']['modify_concentration_ranges_in_reverse'] = False
+    t3 = T3(**input_dict)
+    t3.iteration = 1
+    t3.set_paths()
+    rmg_simulator_adapter = RMGConstantTP(t3=t3.t3,
+                                          rmg=t3.rmg,
+                                          paths=t3.paths,
+                                          logger=t3.logger,
+                                          atol=t3.rmg['model']['atol'],
+                                          rtol=t3.rmg['model']['rtol'],
+                                          observable_list=list(),
+                                          )
+    species_lists = rmg_simulator_adapter.get_species_concentration_lists_from_ranged_params()
+    assert species_lists == [[{'concentration': 14.1, 'label': 'N2'},
+                              {'concentration': 3.75, 'label': 'O2'},
+                              {'concentration': 1, 'label': 'fuel'}],
+                             [{'concentration': 28.199999999999996, 'label': 'N2'},
+                              {'concentration': 7.5, 'label': 'O2'},
+                              {'concentration': 1, 'label': 'fuel'}],
+                             [{'concentration': 42.3, 'label': 'N2'},
+                              {'concentration': 11.25, 'label': 'O2'},
+                              {'concentration': 1, 'label': 'fuel'}]]
+
+    input_dict = RANGED_INPUT_DICT_2.copy()
+    input_dict['rmg']['species'] = [
+        {'label': 'FA', 'smiles': 'OC=O', 'concentration': [1, 4], 'reactive': True, 'balance': False},
+        {'label': 'O2', 'smiles': '[O][O]', 'concentration': 1, 'reactive': True, 'balance': False},
+        {'label': 'N2', 'smiles': 'N#N', 'concentration': 3.76, 'reactive': False, 'balance': False}]
+    input_dict['t3']['options']['modify_concentration_ranges_together'] = True
+    input_dict['t3']['options']['modify_concentration_ranges_in_reverse'] = False
+    t3 = T3(**input_dict)
+    t3.iteration = 1
+    t3.set_paths()
+    rmg_simulator_adapter = RMGConstantTP(t3=t3.t3,
+                                          rmg=t3.rmg,
+                                          paths=t3.paths,
+                                          logger=t3.logger,
+                                          atol=t3.rmg['model']['atol'],
+                                          rtol=t3.rmg['model']['rtol'],
+                                          observable_list=list(),
+                                          )
+    species_lists = rmg_simulator_adapter.get_species_concentration_lists_from_ranged_params()
+    assert species_lists == [[{'label': 'N2', 'concentration': 3.76},
+                              {'label': 'O2', 'concentration': 1.0},
+                              {'label': 'FA', 'concentration': 1.0}],
+                             [{'label': 'N2', 'concentration': 3.76},
+                              {'label': 'FA', 'concentration': 2.5},
+                              {'label': 'O2', 'concentration': 1.0}],
+                             [{'label': 'FA', 'concentration': 4.0},
+                              {'label': 'N2', 'concentration': 3.76},
+                              {'label': 'O2', 'concentration': 1.0}]]
+
+
 def teardown_module():
     """
     A method that is run after all unit tests in this class.
     Delete all project directories created during these unit tests
     """
-    solver_directory = os.path.join(TEST_DIR, 'iteration_0', 'RMG', 'solver')
-    species_directory = os.path.join(TEST_DIR, 'iteration_0', 'RMG', 'species')
-    sa_directory = os.path.join(TEST_DIR, 'iteration_0', 'SA')
-    log_archive = os.path.join(TEST_DIR, 'log_archive')
-    dirs = [solver_directory, species_directory, sa_directory, log_archive]
-    for dir in dirs:
-        if os.path.isdir(dir):
-            shutil.rmtree(dir, ignore_errors=True)
-    files = [os.path.join(TEST_DIR, 't3.log')]
-    for file in files:
-        if os.path.isfile(file):
-            os.remove(file)
+    for test_dir in [TEST_DIR_1, TEST_DIR_2, TEST_DIR_3]:
+        solver_directory = os.path.join(test_dir, 'iteration_1', 'RMG', 'solver')
+        species_directory = os.path.join(test_dir, 'iteration_1', 'RMG', 'species')
+        sa_directory = os.path.join(test_dir, 'iteration_1', 'SA')
+        log_archive = os.path.join(test_dir, 'log_archive')
+        dirs = [solver_directory, species_directory, sa_directory, log_archive]
+        for dir in dirs:
+            if os.path.isdir(dir):
+                shutil.rmtree(dir, ignore_errors=True)
+        files = [os.path.join(test_dir, 't3.log')]
+        for file in files:
+            if os.path.isfile(file):
+                os.remove(file)
